4c6cf07ff7507ebad2e1befaef6b845681ca0c03
mechztcc <dojathan@gmail.com>
2024-01-24 08:26:11 -0500

    feat: added products on list all orders

--- src/orders/services/find-by-user/find-by-user.service.ts
+++ src/orders/services/find-by-user/find-by-user.service.ts
@@ -6,7 +6,10 @@ export class FindByUserService {
   constructor(private readonly prisma: PrismaService) {}
 
   async execute(id: string) {
-    const orders = await this.prisma.order.findMany({ where: { userId: id } });
+    const orders = await this.prisma.order.findMany({
+      where: { userId: id },
+      include: { products: true },
+    });
     return orders;
   }
 }
